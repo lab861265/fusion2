fd98e3af2a5a361c1fb44fbdeaf2462cdf7fe3f2
lab861265 <152097896+lab861265@users.noreply.github.com>
2024-03-18 01:42:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def convert_to_720p(input_path):
         '-i', renamed_path
     ]
     needPro = 0
-    if resolution[0] >= 1280 and resolution[1] >= 720:
+    if resolution[0] >= 1920:
         ffmpeg_command.append('-vf')
         ffmpeg_command.append('scale=trunc(iw/4)*2:trunc(ih/4)*2')
         needPro = 1
