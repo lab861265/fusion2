f1bf2d792b001049549fe2aeeff20ef02950734a
lab861265 <152097896+lab861265@users.noreply.github.com>
2024-12-09 21:35:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -356,7 +356,7 @@ def proc_media(media_filename, face_filename, out_file_path, is_enhancement, nee
     #clip = VideoFileClip(media_filename)
     #duration = clip.duration
 
-    outTime = need_credit * 30
+    outTime = need_credit * 1000
 
     #if media_filename.lower().endswith(('.jpg')):
     #    outTime = 200
