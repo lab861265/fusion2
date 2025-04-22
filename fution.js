9f7dc14b8a92290e56847abb7afaa9dd622b83af
lab861265 <152097896+lab861265@users.noreply.github.com>
2025-04-22 02:07:46 +0200

    Update fution.js

--- fution.js
+++ fution.js
@@ -48,7 +48,7 @@ function runCmd(cmd, args){
         timeoutTimer = setTimeout(() => {
             const currentTime = Date.now();
             if (currentTime - lastDataTime > timeoutDuration) {
-                if(lastLog.indexOf('Processing') >= 0 || lastLog.indexOf('Downloading') >= 0){
+                if(lastLog.indexOf('Processing') >= 0 || lastLog.indexOf('Downloading') >= 0 || lastLog == ""){
                     console.error('执行超时（35分钟无数据）：强制终止进程');
                     ffmpegProcess.kill('SIGKILL'); // 强制终止进程
                 }else{
